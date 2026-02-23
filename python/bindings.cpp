c3bfa626dbe27a7a9ba159c99c964faec842198e
Ashley Ruglys <ashley.ruglys@gmail.com>
2026-02-23 19:41:19 +0100

    Update bindings

--- python/bindings.cpp
+++ python/bindings.cpp
@@ -6,18 +6,15 @@ namespace py = pybind11;
 
 PYBIND11_MODULE(frostpack, m) {
     py::class_<frostpack::BitArray2D>(m, "BitArray2D")
-        .def(py::init<>())
         .def("sort_key", &frostpack::BitArray2D::sort_key)
         .def_readonly("width", &frostpack::BitArray2D::width)
         .def_readonly("height", &frostpack::BitArray2D::height)
         .def_readonly("uv_min", &frostpack::BitArray2D::uv_min);
 
     py::class_<frostpack::Atlas>(m, "Atlas")
-        .def(py::init<>())
-        .def("place", &frostpack::Atlas::place, py::arg("mask"));
+        .def(py::init<uint32_t>())
+        .def("place", &frostpack::Atlas::place, py::arg("mask"))
+        .def_readonly("array", &frostpack::Atlas::array);
 
-    m.def(
-        "raster_island",
-        &frostpack::raster_island,
-        py::arg("tris"));
+    m.def("raster_island", &frostpack::raster_island, py::arg("tris"));
 }
